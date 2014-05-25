4612e7e3da13b9ec1ad9ab9eab0da6fc5b309b3f
tarun <tarunsharma1691@gmail.com>
2014-05-26 00:47:29 +0530

    testing

---
+++ src/org/tarun/spring/TestingPlatform.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.tarun.spring;
+
+/**
+ * @author tarun
+ * 
+ */
+public class TestingPlatform {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+
+	}
+
+}
